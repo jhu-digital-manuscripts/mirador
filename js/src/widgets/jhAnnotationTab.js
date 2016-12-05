c51728aadc234a43f7f4dbeab7ee420166a2ff09
John Abrahams <jabrah23@gmail.com>
2016-12-05 15:07:36 -0500

    Remove annotation list loading code from annotation tab, now use built in mechanism in window.js

--- js/src/widgets/jhAnnotationTab.js
+++ js/src/widgets/jhAnnotationTab.js
@@ -1,28 +1,11 @@
 (function($) {
 
  $.JhAnnotationTab = function(options) {
-    // jQuery.extend(true, this, {
-    //   element: null,
-    //   appendTo: null,
-    //   windowId: null,
-    //   tabId: null,
-    //   manifest: null,           // Manfist object
-    //   currentCanvasID: null,    // ID of canvas currently being displayed
-    //   visible: null,
-    //   message: {
-    //     error: '<h1 class="error">Failed to load annotation list.</h1>',
-    //     empty: '<h1 class="empty">No annotations available.</h1>',
-    //     noLists: '<h1 class="empty">No annotations found.</h1>',
-    //   },
-    //   pendingRequests: {},
-    // }, options);
-
     this.element = options.element;
     this.appendTo = jQuery(options.appendTo);
     this.windowId = options.windowId;
     this.tabId = options.tabId;
     this.manifest = options.manifest;
-    this.currentCanvasID = options.currentCanvasID;
     this.visible = options.visible;
     this.pendingRequests = {};
     this.message = {
@@ -37,15 +20,17 @@
   $.JhAnnotationTab.prototype = {
     init: function() {
       console.assert(this.manifest, '[jhAnnotationTab] Manifest must be provided.');
-      console.assert(this.currentCanvasID, '[jhAnnotationTab] Current canvas ID must be defined.');
       this.registerWidget();
       this.element = jQuery(this.template()).appendTo(this.appendTo);
       this.bindEvents();
-      this.processCurrentManifest();
     },
 
     bindEvents: function() {
       var _this = this;
+
+      jQuery.subscribe("annotationListLoaded." + this.windowId, function(event, data) {
+        _this.processAnnotationList(data.annotations);
+      });
 
       jQuery.subscribe('tabSelected.' + this.windowId, function(event, data) {
         if (data.id === _this.tabId) {
@@ -54,72 +39,6 @@
           _this.element.hide();
         }
       });
-
-      jQuery.subscribe('currentCanvasIDUpdated.' + _this.windowId, function(event, canvasId) {
-        _this.currentCanvasID = canvasId;
-        _this.processCurrentManifest();
-      });
-    },
-
-    processCurrentManifest: function() {
-      this.loadAnnotationLists(
-        this.manifest.getAnnotationLists(this.currentCanvasID),
-        true
-      );
-    },
-
-    loadAnnotationLists: function(lists, clearAnnotations) {
-      var _this = this;
-
-      if (clearAnnotations) {
-        this.appendTo.find('.annotations').empty();
-      }
-      // Remove any error messages, if necessary
-      this.appendTo.find('.messages').empty();
-
-      if (!lists) {
-        jQuery(this.message.noLists).appendTo(this.element.find('.messages'));
-        return;
-      }
-      console.assert(typeof lists === 'string' || Array.isArray(lists),
-        "[jhAnnotationTab#loadAnnotationLists] provided 'lists' must be a single string, or array of strings.");
-
-      if (typeof lists === 'string') {
-        this.requestList(lists);
-      } else {
-        lists.forEach(function(list) {
-          _this.requestList(list);
-        });
-      }
-    },
-
-    requestList: function(listId) {
-      var _this = this;
-
-      // If there is already a request for this annotation list
-      // do nothing but wait for it to complete
-      if (this.pendingRequests[listId]) {
-        return;
-      }
-
-      this.pendingRequests[listId] =
-        jQuery.ajax({
-          url:   listId,
-          dataType: 'json'
-        })
-        .done(function(data) {
-          _this.processAnnotationList(data);
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-          console.log('[jhAnnotationTab#requestList] Failed to load annotation list. (' + listId + '): ' + errorThrown);
-          jQuery(_this.message.error).appendTo(this.appendTo.find('.messages'));
-        })
-        .always(function() {
-          if (_this.pendingRequests[listId]) {
-            // Remove key/value from pendingRequests
-            _this.pendingRequests[listId] = undefined;
-          }
-        });
     },
 
     /**
@@ -132,13 +51,16 @@
       var annotations = [];
       var appendTo = this.appendTo.find('ul.annotations');
 
-      if (!annotationList.resources || annotationList.resources.length === 0) {
+      appendTo.empty();
+      appendTo.scrollTop(0);
+
+      if (!annotationList || annotationList.length === 0) {
         jQuery(this.message.empty).appendTo(this.appendTo.find('.messages'));
       }
 
       // Massage data slightly, Handlebars cannot deal with weird JSON-LD
       // properties such as '@id', just change these to 'id'
-      annotationList.resources.forEach(function(annotation) {
+      annotationList.forEach(function(annotation) {
         if (annotation['@type'] !== 'oa:Annotation') {
           return;
         }
